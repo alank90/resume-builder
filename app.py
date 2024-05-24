c7b99bde74606f69737eb0a98e3c21c3b727f9d8
Alan K <akillian@outlook.com>
2024-05-24 11:23:16 -0400

    Added pdf file creation

--- app.py
+++ app.py
@@ -1,55 +1,10 @@
 import requests
+import create_pdf
 
-
-def convert_markdown_to_pdf(markdown_content, resume_file="Resume.pdf", engine="weasyprint"):
-    # Define CSS styles for the PDF
-    css_file = """
-                body{
-                    padding: 0px;
-                    margin:0px;
-                }
-                h1 {
-                color: MidnightBlue;
-                margin:0px;
-                padding:0px;
-
-                }
-                h3{
-                    color: MidnightBlue;
-                    padding-bottom:0px;
-                    margin-bottom:0px;
-                }
-                li{
-                    margin-top:5px;
-                }
-
-                """
-
-    # API endpoint for converting Markdown to PDF
-    url_address = "https://md-to-pdf.fly.dev"
-
-    # Data to be sent in the POST request
-    data = {
-        'markdown': markdown_content,
-        'css': css_file,
-        'engine': engine
-    }
-
-    # Send a POST request to the API
-    response = requests.post(url_address, data=data)
-
-    # Check if the response is successful (status code 200)
-    if response.status_code == 200:
-        # Save the generated PDF to a file
-        with open(resume_file, 'wb') as f:
-            f.write(response.content)
-        print(f"PDF saved to {resume_file}")
-    else:
-        print(f"Error {response.status_code}: {response.text}")
 
 # pylint: disable-next=too-many-instance-attributes,too-few-public-methods
 
-
+# ============== Class object Resume ================================= #
 class Resume:
     # pylint: disable-next=too-many-arguments
     def __init__(self, name,  email, mobile, education, skills,
@@ -101,7 +56,12 @@ class Resume:
         markdown_text += self.activities + '\n'
 
         return markdown_text
+# ============== End of class object Resume ========================== #
 
+
+# ==================================================================== #
+# ============== Function Declarations =============================== #
+# ==================================================================== #
 
 def get_user_input():
     # Gather user input for creating the resume
@@ -176,13 +136,71 @@ def get_user_input():
     return Resume(name, email, mobile, education, skills, experience, projects, achievements, activities)
 
 
-if __name__ == "__main__":
-    # Main execution block
+def convert_markdown_to_pdf(markdown_content, resume_file, engine="weasyprint"):
+    # Define CSS styles for the PDF
+    css_file = """
+                body{
+                    padding: 0px;
+                    margin:0px;
+                }
+                h1 {
+                color: MidnightBlue;
+                margin:0px;
+                padding:0px;
 
-    # Create a .pdf file
-    resume_file_name = input("Give me a name for your resume file(include .pdf extention: ")
-    print(resume_file_name)
-    exit
+                }
+                h3{
+                    color: MidnightBlue;
+                    padding-bottom:0px;
+                    margin-bottom:0px;
+                }
+                li{
+                    margin-top:5px;
+                }
+
+                """
+
+    # API endpoint for converting Markdown to PDF
+    url_address = "https://md-to-pdf.fly.dev"
+
+    # Data to be sent in the POST request
+    data = {
+        'markdown': markdown_content,
+        'css': css_file,
+        'engine': engine
+    }
+
+    # Send a POST request to the API
+    response = requests.post(url_address, data=data)
+
+    # Check if the response is successful (status code 200)
+    if response.status_code == 200:
+        # Save the generated PDF to a file
+        resume_file.write(response.content)
+        print(f"PDF saved to {resume_file}")
+    else:
+        print(f"Error {response.status_code}: {response.text}")
+
+# ==================================================================== #
+# ============== End of Function Declarations ======================== #
+# ==================================================================== #
+
+# -------------------------------------------------------------------------------- #
+
+
+# =========== Begin Main execution ================================ #
+if __name__ == "__main__":
+    # Create the .pdf file
+    create_pdf
+
+    # ============================================================= #
+    # ============= Main execution block ========================== #
+    # ============================================================= #
+
     user_resume = get_user_input()
     markdown_text = user_resume.generate_markdown()
-    convert_markdown_to_pdf(markdown_text)
+    convert_markdown_to_pdf(markdown_text, create_pdf.f)
+
+    # ============================================================= #
+    # ============= End of Main execution block =================== #
+    # ============================================================= #
---
+++ create_pdf.py
@@ -0,0 +1,5 @@
+# Create a .pdf file
+resume_file_name = input(
+    "Give me a name for your resume file(include .pdf extention: ")
+print(resume_file_name)
+f = open(resume_file_name, 'wb')
